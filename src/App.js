79b44487f7eb4e2d62de9b55a37244c351338926
Saurabh Kumar Singh <singhsaurabh9451985554@gmail.com>
2022-07-21 21:06:20 +0530

    day3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Cart from "./components/cart/Cart";
+import Checkout from "./components/cart/Checkout";
 import Login from "./components/Login & Signup/Login/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      {/* <Cart/> */}
+      <Checkout/>
+      {/* <Navbar/> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import { store } from "./Redux/store.js";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  // <BrowserRouter>
     <Provider store={store}>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </Provider>
-  </BrowserRouter>
+  //  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
