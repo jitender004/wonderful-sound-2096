7a0b3e851cd504cce4de0f45774658e8761f1d5b
arunrajbhar123 <arunrajbhar1998@gmail.com>
2022-07-22 15:20:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -5,20 +5,16 @@ import Checkout from "./components/cart/Checkout";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login & Signup/Login/Login";
 import PcPerks from "./components/Pc-Perks/PcPerks";
-<<<<<<< HEAD
 
-
-=======
-import Home from "./pages/Home";
->>>>>>> bf58ce23af8032c9facddc349d681b4eb495830f
 function App() {
   return (
     <div className="App">
+      <Login />
       {/* <Cart/> */}
       {/* <Checkout /> */}
       {/* <Navbar/> */}
       {/* <Login /> */}
-      <Home />
+
       {/* <BestSeller /> */}
       {/* <PcPerks /> */}
     </div>
